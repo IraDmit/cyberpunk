f3c85208a84107fca3fa29fac2e720fe15036f72
IraDmitrieva <iritrieva@gmail.com>
2023-09-18 19:22:47 +0400

    slider header

---
+++ src/components/app-slider.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <VueSlickCarousel class="slider">
+      <div
+        v-for="(slide, idx) in sliderList"
+        :key="'slider' + idx"
+        class="slide"
+      >
+        <img :src="slide.img" :alt="slide.img" />
+      </div>
+    </VueSlickCarousel>
+  </div>
+</template>
+
+<script>
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+// optional style for arrows & dots
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+
+export default {
+  name: "app-slider",
+  components: { VueSlickCarousel },
+  data() {
+    return {
+      settings: {
+        dots: true,
+        infinite: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 500,
+        pauseOnDotsHover: true,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+      },
+      sliderList: [
+        {
+          img: require("../assets/images/11.jpg"),
+        },
+        {
+          img: require("../assets/images/12.jpg"),
+        },
+        {
+          img: require("../assets/images/Untitled-1 2.jpg"),
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.slide {
+  img {
+    object-fit: cover;
+  }
+}
+</style>
---
+++ src/navigation/the-header.vue
@@ -0,0 +1,67 @@
+<template>
+  <header >
+    <div class="header-wrp container">
+      <div class="logo">
+        <img
+          src="../assets/images/Cyberpunk_2077_logo 1.svg"
+          alt="../assets/images/Cyberpunk_2077_logo 1.svg"
+        />
+      </div>
+      <ul class="header-logos">
+        <li class="messenger">
+          <img
+            src="../assets/images/youtube.svg"
+            alt="../assets/images/youtube.svg"
+          />
+        </li>
+        <li class="messenger">
+          <img src="../assets/images/vk.svg" alt="../assets/images/vk.svg" />
+        </li>
+        <li class="messenger">
+          <img
+            src="../assets/images/facebook.svg"
+            alt="../assets/images/facebook.svg"
+          />
+        </li>
+        <li class="messenger">
+          <img
+            src="../assets/images/twitter.svg"
+            alt="../assets/images/twitter.svg"
+          />
+        </li>
+        <li class="messenger">
+          <img
+            src="../assets/images/twitch.svg"
+            alt="../assets/images/twitch.svg"
+          />
+        </li>
+        <li class="messenger">
+          <img
+            src="../assets/images/instagram.svg"
+            alt="../assets/images/instagram.svg"
+          />
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  width: 100vw; 
+  .header-wrp {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .header-logos {
+    display: flex;
+    gap: 40px;
+    margin-left: auto;
+  }
+}
+</style>
